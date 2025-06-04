a160cd6777ff6bdcbd7051a8d21d4bb33d45eb9c
BlackB34rd <contact@the-tiny-spark.com>
2025-06-04 16:25:16 +0200

    add guard testing and spies

--- src/app/guards/is-connected.guard.spec.ts
+++ src/app/guards/is-connected.guard.spec.ts
@@ -9,7 +9,7 @@ import {
 import { isConnectedGuard } from './is-connected.guard';
 import { importProvidersFrom } from '@angular/core';
 import { AppService } from '../services/app.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+// import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { of } from 'rxjs';
 import { Product } from '../models/product.model';
 
@@ -17,8 +17,9 @@ fdescribe('isConnectedGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => isConnectedGuard(...guardParameters));
 
-  var httpClient: HttpClient;
-  var service: AppService;
+  // var httpClient: HttpClient;
+
+
   var activatedRoute = {} as ActivatedRouteSnapshot;
   var routeSnapshot = {} as RouterStateSnapshot;
   const PRODUCTS_MOCK: Product[] = [
@@ -40,12 +41,26 @@ fdescribe('isConnectedGuard', () => {
     },
   ];
 
+  var service: any = {
+    connected: true,
+    cart: PRODUCTS_MOCK,
+    isConnected: ()=>{
+      return Promise.resolve(service.connected)
+    },
+    getCart: ()=>{
+      return of(service.cart)
+    }
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [importProvidersFrom(HttpClientModule)],
+      providers: [
+        /*importProvidersFrom(HttpClientModule)*/
+        {provide: AppService, useValue: service},
+      ],
     });
-    httpClient = TestBed.inject(HttpClient);
-    service = TestBed.inject(AppService);
+    // httpClient = TestBed.inject(HttpClient);
+    // service = TestBed.inject(AppService);
   });
 
   it('should be created', () => {
@@ -54,20 +69,28 @@ fdescribe('isConnectedGuard', () => {
 
   it('should return true if isConnected = true and there is a non empty cart', async () => {
     // given
-    const spy = spyOn(service, 'isConnected').and.resolveTo(true);
-    const spy2 = spyOn(service, 'getCart').and.returnValue(of(PRODUCTS_MOCK));
+    // const spy = spyOn(service, 'isConnected').and.resolveTo(true);
+    // const spy2 = spyOn(service, 'getCart').and.returnValue(of(PRODUCTS_MOCK));
+    service.connected = true;
+    service.cart = PRODUCTS_MOCK;
 
     //when
     const result = await executeGuard(activatedRoute, routeSnapshot);
 
     //then
     expect(result).toBeTrue();
+
+    // on peut vérifier que le spy a été appelé
+    // expect(spy).toHaveBeenCalled();
+    // expect(spy).toHaveBeenCalledOnceWith();
   });
 
   it('should return false if isConnected = true and there is an empty cart', async () => {
     // given
-    const spy = spyOn(service, 'isConnected').and.resolveTo(true);
-    const spy2 = spyOn(service, 'getCart').and.returnValue(of([]));
+    // const spy = spyOn(service, 'isConnected').and.resolveTo(true);
+    // const spy2 = spyOn(service, 'getCart').and.returnValue(of([]));
+    service.connected = true;
+    service.cart = [];
 
     // when
     const result = await executeGuard(activatedRoute, routeSnapshot);
@@ -78,8 +101,10 @@ fdescribe('isConnectedGuard', () => {
 
   it('should return false if isConnected = false and there is a non empty cart', async () => {
     // given
-    const spy = spyOn(service, 'isConnected').and.resolveTo(false);
-    const spy2 = spyOn(service, 'getCart').and.returnValue(of(PRODUCTS_MOCK));
+    // const spy = spyOn(service, 'isConnected').and.resolveTo(false);
+    // const spy2 = spyOn(service, 'getCart').and.returnValue(of(PRODUCTS_MOCK));
+    service.connected = false;
+    service.cart = PRODUCTS_MOCK;
 
     // when
     const result = await executeGuard(activatedRoute, routeSnapshot);
--- src/app/guards/is-connected.guard.ts
+++ src/app/guards/is-connected.guard.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { AppService } from '../services/app.service';
 import { firstValueFrom } from 'rxjs';
 
