cdf9cbc0600f5224f1ac30bd27cb37d26339b3e8
BlackB34rd <contact@the-tiny-spark.com>
2025-09-22 15:27:50 +0200

    ajout de getProductById et de firstValueFrom

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,26 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../models/product.model';
+import { NgIf } from '@angular/common';
+import { AppService } from '../../services/app.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
   public productId:number = 0;
+  public currentProduct:Product|null = null;
+  private _appService:AppService = inject(AppService);
 
   constructor( private _route:ActivatedRoute){
-
   }
 
-  ngOnInit(){
+  async ngOnInit(){
+
+    // version 100% observable
     this._route.paramMap.subscribe(
        (params)=>{
         this.productId = parseInt(params.get('id') || "-1");
+        this._appService.getProductById(this.productId).subscribe( 
+          (data:Product|null)=>{
+            this.currentProduct = data;
+          }
+        );
        }
     );
+
+    // version avec les promesses
+    const params = await firstValueFrom( this._route.paramMap );
+    this.productId = parseInt(params.get('id') || "-1");
+    this.currentProduct = await this._appService.getProductByIdPromise(this.productId);
   }
 }
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
-import { Observable } from 'rxjs';
+import { firstValueFrom, map, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -17,5 +17,18 @@ export class AppService {
 
   public getCart():Observable<Product[]>{
     return this.httpService.get<Product[]>("assets/json/cart.json");
+  }
+
+  public getProductById( id:number ):Observable<Product|null>{
+    return this.getProducts().pipe( map(
+      (products:Product[])=>{
+        return products.find( (p)=>p.id == id ) || null;
+      }
+    ));
+  }
+
+  public getProductByIdPromise( id:number ):Promise<Product|null>{
+    // on transforme notre observable en promise
+    return firstValueFrom(this.getProductById(id));
   }
 }
