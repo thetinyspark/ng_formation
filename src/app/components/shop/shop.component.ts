64fab04b13fe11b30198aa092945afe17384f6f6
BlackB34rd <contact@the-tiny-spark.com>
2025-09-22 15:55:18 +0200

    put catalog on shopcomponent

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CatalogComponent } from '../catalog/catalog.component';
+import { NgForOf } from '@angular/common';
+import { AppService } from '../../services/app.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, CatalogComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent {
 
+  private _appService = inject(AppService);
+  public products:Product[] = [];
+
+  constructor(){}
+  ngOnInit(){
+    this._appService.getProducts().subscribe(
+      (data:Product[])=>{
+        this.products = data;
+      }
+    );
+  }
 }
