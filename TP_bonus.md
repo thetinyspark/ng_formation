aab5b3e750df617ad25d22aa7d72d8cde34323c3
BlackB34rd <contact@the-tiny-spark.com>
2025-09-23 09:45:39 +0200

    add tp bonus

---
+++ TP_bonus.md
@@ -0,0 +1,10 @@
+Modifier le catalogue de sorte à ce qu'il y ait un filtre :
+ 
+- Qui permette de montrer uniquement les produits en tendance (ou non) (checkbox suffit)
+- Qui permet de montrer les produits au dessus d'un certain prix
+- Qui permet de montrer les produits en dessous d'un certain prix
+ 
+ 
+BONUS:
+ 
+Qui permette de montrer les produits par plateforme (PS5, Steam)
--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { CatalogComponent } from '../catalog/catalog.component';
 import { NgForOf } from '@angular/common';
 import { AppService } from '../../services/app.service';
 import { Product } from '../../models/product.model';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-shop',
@@ -14,14 +15,58 @@ import { Product } from '../../models/product.model';
 export class ShopComponent {
 
   private _appService = inject(AppService);
-  public products:Product[] = [];
+  private _priceMin = signal<number>(0);
+  private _priceMax = signal<number>(100);
+  private _isTrendy = signal<boolean>(false);
+  private _platform = signal<string>("All");
+  private _allProducts = signal<Product[]>([]);
 
-  constructor(){}
-  ngOnInit(){
-    this._appService.getProducts().subscribe(
-      (data:Product[])=>{
-        this.products = data;
+  public products:Product[] = [];
+  public platforms:string[] = ["All", "PS5", "STEAM"];
+
+  constructor(){
+    effect( 
+      ()=>{
+
+        const min = this._priceMin();
+        const max = this._priceMax();
+        const trendy = this._isTrendy();
+        const platform = this._platform();
+        const products = this._allProducts();
+        this.products = products.filter( 
+          (product:Product)=>{
+            if( trendy && !product.trendy)
+              return false;
+
+            if( platform != "All" && product.device != platform)
+              return false;
+
+            return ( product.price >= min && product.price <= max );
+          }
+        );
+
       }
     );
+  }
+
+  async ngOnInit(){
+    this._allProducts.set( await firstValueFrom( this._appService.getProducts() ) );
+  }
+  
+
+  onPriceMinChange( data:any){ 
+    this._priceMin.set( parseInt( data.value ) );
+  }
+
+  onPriceMaxChange( data:any){ 
+    this._priceMax.set( parseInt( data.value ) );
+  }
+
+  onTrendChange( data:any){ 
+    this._isTrendy.set( data.checked );
+  }
+
+  onPlatformChange( data:any ){
+    this._platform.set( data.value );
   }
 }
